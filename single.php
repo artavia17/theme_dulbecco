fd0b514fe309626f2955c7d23a13cdb163ccf0fb
Alonso-GarnierBBDO <artaviaalonso60@gmail.com>
2024-04-18 22:36:24 -0600

    Se ajusto el sitio web

--- single.php
+++ single.php
@@ -77,15 +77,18 @@
                                                 <img src="<?= get_theme_file_uri('/assets/image/logo_dulbeco.png') ?>" alt="Dulbecco Logo">
                                             </span>
                                             <img class="ilustracion" src="<?= $image ?>" alt="Ilustracion dulbecco">
-                                            <button>Up Scale</button>
+                                            <button>
+                                                Up Scale
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-fullscreen" viewBox="0 0 16 16">
+                                                    <path fill-rule="evenodd" d="M5.828 10.172a.5.5 0 0 0-.707 0l-4.096 4.096V11.5a.5.5 0 0 0-1 0v3.975a.5.5 0 0 0 .5.5H4.5a.5.5 0 0 0 0-1H1.732l4.096-4.096a.5.5 0 0 0 0-.707m4.344 0a.5.5 0 0 1 .707 0l4.096 4.096V11.5a.5.5 0 1 1 1 0v3.975a.5.5 0 0 1-.5.5H11.5a.5.5 0 0 1 0-1h2.768l-4.096-4.096a.5.5 0 0 1 0-.707m0-4.344a.5.5 0 0 0 .707 0l4.096-4.096V4.5a.5.5 0 1 0 1 0V.525a.5.5 0 0 0-.5-.5H11.5a.5.5 0 0 0 0 1h2.768l-4.096 4.096a.5.5 0 0 0 0 .707m-4.344 0a.5.5 0 0 1-.707 0L1.025 1.732V4.5a.5.5 0 0 1-1 0V.525a.5.5 0 0 1 .5-.5H4.5a.5.5 0 0 1 0 1H1.732l4.096 4.096a.5.5 0 0 1 0 .707"/>
+                                                </svg>
+                                            </button>
                                         </section>
-                                        <section class="content">
+                                        <section class="content responsive-box">
                                             <section class="description">
-                                                <h5>Description: </h5>
                                                 <?= $descripcion ?>
                                             </section>
                                             <section class="sinopsis">
-                                                <h5>Synopsis: </h5>
                                                 <?= $sinopsis ?>
                                             </section>
                                         </section>
